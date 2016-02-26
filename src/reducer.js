4845e49be0b34ae7c0d73e79e9ffa4bd6cf63e15
krzychukula <krzychukula@gmail.com>
2016-02-26 07:24:07 +0100

    Use connect to create FilterLink 29/30

--- src/reducer.js
+++ src/reducer.js
@@ -155,40 +155,33 @@ const Link = ({
   )
 }
 
-class FilterLink extends Component {
-  componentDidMount () {
-    const { store } = this.context
-    this.unsubscribe = store.subscribe(() => {
-      this.forceUpdate()
-    })
+const mapStateToLinkProps = (
+  state,
+  ownProps
+) => {
+  return {
+    active: ownProps.filter === state.visibilityFilter
   }
-  componentWillUnmount () {
-    this.unsubscribe()
-  }
-  render () {
-    const props = this.props
-    const { store } = this.context
-    const state = store.getState()
+}
 
-    return (
-      <Link
-        active={
-          props.filter === state.visibilityFilter
-        }
-        onClick={() =>
-          store.dispatch({
-            type: 'SET_VISIBILITY_FILTER',
-            filter: props.filter
-          })
-        } >
-          {props.children}
-        </Link>
-    )
+const mapDispatchToLinkProps = (
+  dispatch,
+  ownProps
+) => {
+  return {
+    onClick: () => {
+      dispatch({
+        type: 'SET_VISIBILITY_FILTER',
+        filter: ownProps.filter
+      })
+    }
   }
 }
-FilterLink.contextTypes = {
-  store: React.PropTypes.object
-}
+
+const FilterLink = connect(
+  mapStateToLinkProps,
+  mapDispatchToLinkProps
+)(Link)
 
 let nextTodoId = 1
 let AddTodo = ({ dispatch }) => {
@@ -218,11 +211,11 @@ const Todo = ({
   text
 }) => {
   return (<li
-       onClick={onClick}
-       style={{
-         textDecoration:
-          completed ? 'line-through' : 'none'
-       }}>
+    onClick={onClick}
+    style={{
+      textDecoration:
+        completed ? 'line-through' : 'none'
+    }}>
     {text}
   </li>)
 }
@@ -233,12 +226,12 @@ const TodoList = ({
 }) => {
   return (
     <ul>
-    {todos.map(todo =>
-      (<Todo key={todo.id}
-      {...todo}
-      onClick={() => onTodoClick(todo.id)} />)
-    )}
-  </ul>)
+      {todos.map(todo =>
+        (<Todo key={todo.id}
+          {...todo}
+          onClick={() => onTodoClick(todo.id)} />)
+      )}
+    </ul>)
 }
 
 const Footer = () => (
